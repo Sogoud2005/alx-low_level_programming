cfb0e12d894ba344ba1199c4654df843972f3629
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 02:19:10 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
-puts("\"Programming is like building a multilingual puzzle"
-		return = (0)
 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle";
+		return (0);
+		}
