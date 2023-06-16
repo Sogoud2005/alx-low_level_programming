8137c8b310b6db5a1c015c246ef9195865c4834c
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 03:56:28 -0700

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - a function that prunts sizes
+ * return: 0 (success)
+*/
+int main(void)
+{
+
+char c;
+int i;
+long int l;
+long long int d;
+float f;
+
+printf("Size of a char: %d %s. \n", (unsigned long)sizeof(c), byte(s))
+printf("Size of an int: %d %s. \n", (unsigned long)sizeof(i), byte(s))
+printf("Size of a long int: %d %s. \n", (unsigned long)sizeof(l), byte(s))
+printf("Size of a long long int: %d %s. \n", (unsigned long)sizeof(d), byte(s))
+printf("Size of a float: %d %s. \n", (unsigned long)sizeof(f), byte(s))
+
+return (0)
+}
+
