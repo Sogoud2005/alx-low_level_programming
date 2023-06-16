324dda8b90a63ad65bcdcdd23da4912f57cb5426
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 04:55:09 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-printf("%s", "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1)
 }
