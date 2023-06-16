23fe72b61526765d9927991ae738c67ded88724d
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 04:53:16 -0700

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that displays an error msg
+ * Return: 1 (Success)
+*/
+int main(void)
+{
+printf("%s", "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1)
+}
