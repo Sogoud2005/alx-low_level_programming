3c226acdb3074a8bb15bd170b0691dbc6546d069
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 22:49:31 -0700

    if..else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+unsigned int num;
+
+while (num < 10)
+{
+printf( "%u\n", num );
+}
+return (0);
+}
