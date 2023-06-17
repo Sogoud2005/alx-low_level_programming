5f4c8a06e8de38cb9f3418225f4657c4c1711552
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 20:34:35 -0700

    if..else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet
+ * Return: 0 (Success)
+*/
+
+int main(void);
+{
+int x;
+x = tolower(x);
+for (int x ; x = 'A' ; x <= 'Z' ; x++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
