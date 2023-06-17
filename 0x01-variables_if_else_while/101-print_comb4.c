17bb2c1dca3ba2ba2a98eeacd1741e43a924efae
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-17 03:50:49 -0700

    if..else

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints numbers
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int h, t, u;
+for (h = '0'; h < '8'; h++)
+{
+for (t = h + 1; t < '9'; t++)
+{
+for (u = h + 2; u <= '9'; u++)
+{
+if (h != t && h != u && t != u)
+{
+putchar(h);
+putchar(t);
+putchar(u);
+if (h == '7' && t == '8' && u == '9')
+continue;
+putchar('\n');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
