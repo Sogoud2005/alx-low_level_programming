0fd2fb3900a873a28040527265c33cdb2ea51524
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 22:24:19 -0700

    if..else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 char alpha;
-for (alpha = 'a'; alpha <= 'z' && alpha != 'e' || 'q'; alpha++)
+for (alpha = 'a'; alpha <= 'z' && alpha != ('e' || 'q'); alpha++)
 {
 putchar(alpha);
 }
