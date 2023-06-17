728ae1e987272ed2151634aef5c320d397960515
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-17 04:36:58 -0700

    if..else

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print numbers
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int n, s, t, f;
+for (n = '0'; n <= '9'; n++)
+{
+for (s = '0'; s < '9'; s++)
+{
+for (t = '0'; t <= '9'; t++)
+{
+for (f = '1'; f <= '9'; f++)
+{
+if (n != t && s != f)
+{
+putchar(n);
+putchar(s);
+putchar(' ');
+putchar(t);
+putchar(f);
+if (n == '9' && s == '8' && t == '9' && f == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n')
+return (0);
+}
