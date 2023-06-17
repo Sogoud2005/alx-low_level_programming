cb122b31779d79d5def7e3be7daffbf8e13b8ec7
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 23:58:43 -0700

    if..else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints digits
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+for (n = '0'; n <= '8'; n++)
+{
+putchar(n',');
+}
+putchar('9');
+putchar('\n');
+return (0);
+}
