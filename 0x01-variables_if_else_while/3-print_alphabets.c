a0208c70faa63e4bff19603a45569261ab0e060b
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-16 21:54:25 -0700

    if..else

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - aprogram that prints the alphabet 
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char alphal;
+char alphau;
+
+for (alphal = 'a'; alphal <= 'z'; alphal++)
+{
+putchar(alphal);
+}
+for (alphau = 'A'; alphau <= 'Z'; alphau++)
+{
+putchar(alphau);
+}
+return (0);
+}
