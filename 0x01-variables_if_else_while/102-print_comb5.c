d14b857dacdcce03f8f3ac064d3e2249fd3a9ff1
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-18 02:20:18 -0700

    if..else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 int f, s;
-for (f = '0'; f <= '98'; f++)
+for (f = '0'; f <= 98; f++)
 {
-for (s = f + 1; s <= '99'; s++)
+for (s = f + 1; s <= 99; s++)
 {
 putchar((f / 10) + '0');
 putchar((f % 10) + '0');
 putchar(' ');
 putchar((s / 10) + '0');
 putchar((s & 10) + '0');
-if (f == '98' && s == '99')
+if (f == 98 && s == 99)
 continue;
 putchar(',');
 putchar(' ');
