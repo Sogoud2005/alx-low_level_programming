20102979a9e95facfd6f8f680b7f20fd1c6b95c4
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-21 00:00:54 -0700

    functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/** 
+ * main - a function that adds
+ * Return: 0
+*/
+int main(void)
+{
+int n, s = 0;
+while (n <= 1024)
+{
+if ((n % 3 == 0) || (n % 5 ==0))
+{
+s += n;
+}
+n++
+}
+printf("%d\n", s);
+return (0);
+}
