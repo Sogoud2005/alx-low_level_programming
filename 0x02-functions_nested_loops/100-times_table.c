88f45beee2a15a18b317494f0cc6271fb6876140
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-20 23:20:46 -0700

    functions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_times_table - prints numbers
+ * @n: a character
+ * Return: nothing
+*/
+void print_times_table(int n)
+{
+int a, b, c;
+if (n <= 15 &&  n >= 0)
+{
+for (a = 0; a <= n; a++)
+{
+_putchar('0');
+for (b = 1; b <= n; n++)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+c = a * b;
+if (c <= 9)
+{
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c > 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
