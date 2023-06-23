e0502d1f04d69011c423a85ba32819efcab37007
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-23 00:01:41 -0700

    functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,14 +9,13 @@ int mul(int a, int b)
 {
 	int m;
 
-	m = a * b;
 	if (a < 0 || b < 0)
 	{
-	_putchar(-m + '0');
+	_putchar(-(a * b) + '0');
 	}
 	else
 	{
-	_putchar(m + '0');
+	_putchar((a * b) + '0');
 	}
 	_putchar('\n');
 	return (0);
