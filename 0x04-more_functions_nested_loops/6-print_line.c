b4a242d54c86d310dc2e30a5bb79e580894357ac
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-23 01:50:32 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - a program
+ * @n: a character
+ * Return: nothing
+*/
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	for (n = 1; n <= n; n++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
