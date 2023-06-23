ed21e89916fbe9cb20278988731394f4b316839e
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-23 01:20:45 -0700

    functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,19 @@
 */
 void more_numbers(void)
 {
-int n, c;
+int n, c, i;
 for (c = 0; c < 10; c++)
 {
-	for (n = 0; n <= 14; n++)
-	{
-		if (n > 9)
-		{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		}
-	}
-		_putchar('\n');
+for (n = 0; n <= 14; n++)
+{
+i = n;
+if (n > 9)
+{
+_putchar(1 + '0');
+i = n % 10
+}
+_putchar(i + '0');
+}
+_putchar('\n');
 }
 }
