a60ae3de404c583a49d052e9847641b77060bf7e
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-22 23:45:55 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - a program
+ * @a: an ascii character
+ * @b: an ascii character
+ * Return: 0
+*/
+int mul(int a, int b)
+{
+	_putchar((a * b) + '0');
+	_putchar('\n');
+	return (0);
+}
