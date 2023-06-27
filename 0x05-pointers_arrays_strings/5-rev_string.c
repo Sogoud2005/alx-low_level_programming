11b150c5be4ae88c729e23597f13871cadee5f46
Sogoud2005 <Sogoud2005@gmail.com>
2023-06-27 03:07:09 -0700

    string, arrays and pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * rev_string - a program
+ * @s: a character
+ * Return: nothing
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	_putchar('\n');
+}
