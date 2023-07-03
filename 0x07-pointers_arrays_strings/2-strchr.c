108808b39500141849fcc3d4d1e89dfd4d4fb5d4
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-03 15:04:12 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strchr - a
+ * @s: ascii
+ * @c: ascii
+ * Return: nothing
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < '\0'; i++)
+	{
+		if (i = 'c')
+		{
+			s = i;
+			return (s);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
