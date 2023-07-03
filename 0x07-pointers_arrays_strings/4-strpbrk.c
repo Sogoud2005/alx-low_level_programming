6f521e5cc69103bd33e4b3452d88b084760bfd23
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-03 16:53:37 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strpbrk - a
+ * @s: ascii
+ * @accept: ascii
+ * Return: nothing
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+	while (accept[j] != '\0')
+	{
+		if (accept[j] == s[i])
+		{
+		p = &s[i];
+		return (p);
+		}
+	}
+	}
+}
