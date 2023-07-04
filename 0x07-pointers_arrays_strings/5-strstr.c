080337e69689fcaffa3caeb87358375d2dbfdcb4
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-03 17:11:35 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strstr - a
+ * @haystack: ascii
+ * @needle: ascii
+ * Return: nothing
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *p;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+		if (needle[j] == haystack[i])
+		{
+			p = &haystack[i];
+			return (p);
+		}
+		j++;
+		}
+		i++
+	}
+	return ('\0');
+}
