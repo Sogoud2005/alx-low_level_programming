6ae36474b7274c2203e6f70420f0b4b3a21b28d1
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-03 17:41:22 -0700

    more pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,22 +7,21 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-	char *p;
-i = 0;
-	while (haystack[i] != '\0')
+	char *p, *i;
+
+	for (haystack != '\0'; haystack++)
 	{
-		j = 0;
-		while (needle[j] != '\0')
+		*p = haystack;
+		*i = needle;
+		while (*p == *i && *i != '\0')
 		{
-		if (needle[j] == haystack[i])
+			*p++;
+			*i++;
+		}
+		if (i == '\0')
 		{
-			p = &haystack[i];
-			return (p);
+			return (haystack);
 		}
-		j++;
-		}
-		i++
 	}
 	return ('\0');
 }
