bf00060e4126921cb275598ffa351b5a6553c4af
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-05 10:39:44 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - a program
+ * @n: ascii
+ * Return: value
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - a program
+ * @n: ascii
+ * @i: ascii
+ * Return: value
+*/
+int _sqrt(int n, int i)
+{
+	int s = i * i;
+
+	if (s < n)
+	{
+		return (-1);
+	}
+	else if (s == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
