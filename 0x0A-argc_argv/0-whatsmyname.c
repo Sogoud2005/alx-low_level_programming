f99769f97d16d12309b53c8d10c93b06a75ba086
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-07 16:54:03 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program
+ * @argc: count
+ * @argv: value
+ * Return: none
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,2 @@
+int main(int argc, char const *argv[]);
+int _putchar(char c);
