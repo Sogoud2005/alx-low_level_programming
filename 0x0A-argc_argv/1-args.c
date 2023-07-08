8903e88c1e601361d0750a39d0e72959dc01b6fa
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-07 17:03:44 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a
+ * @argc: a
+ * @argv: a
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
