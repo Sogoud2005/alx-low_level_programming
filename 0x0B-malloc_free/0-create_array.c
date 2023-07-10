00f366c9a849297ca8d7603ec13c13e0ade63652
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-10 16:09:47 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *create_array - a
+ * @size: ascii
+ * @c: ascii
+ * Return: value
+*/
+char *create_array(unsigned int size, char c)
+{
+	if (size > 0)
+	{
+		int i = size;
+
+	char *p;
+
+	p = malloc(i * sizeof(char));
+
+	p[0] = c;
+
+	return (p);
+	}
+	else
+	{
+		return(NULL);
+	}
+	void free(p);
+}
