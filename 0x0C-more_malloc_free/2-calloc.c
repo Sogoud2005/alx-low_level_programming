3dde12ca85b2ec501803dd9d49cd85f7a0553b0f
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-13 20:00:47 -0700

    malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - a
+ * @nmemb: a
+ * @size: a
+ * Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	return (p);
+}
