4f7bc777bb71654d890252e86179e12d368c399c
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-14 17:39:36 -0700

    malloc, free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *_realloc - a
+ * @ptr: a
+ * @old_size: a
+ * @new_size: a
+ * Return: pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *m;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL);
+		return (m);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		m = free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)m + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (m);
+}
