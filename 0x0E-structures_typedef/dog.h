b0d621e69403083803e538b0460520d214e59958
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-18 15:30:56 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,10 @@ typedef struct dog
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef dog_t
+{
+	char *name;
+        float age;
+        char *owner;
+};
 #endif
