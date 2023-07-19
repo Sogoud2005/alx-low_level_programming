b24ee98bbf73b02c9cec0d1ae3ea0dad36d7e332
Sogoud2005 <Sogoud2005@gmail.com>
2023-07-19 15:42:44 -0700

    functions pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - a
+ * @a: a
+ * @b: a
+ * Return: a
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a
+ * @a: a
+ * @b: a
+ * Return: a
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a
+ * @a: a
+ * @b: a
+ * Return: a
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a
+ * @a: a
+ * @b: a
+ * Return: a
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a
+ * @a: a
+ * @b: a
+ * Return: a
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
