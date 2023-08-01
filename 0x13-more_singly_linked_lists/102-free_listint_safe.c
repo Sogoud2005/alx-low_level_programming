66630a369a32649b3599a73941fd9be768b32e04
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-01 10:13:25 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,32 +3,26 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * r - reallocates memory
- * @list: old list
- * @size: size of the list
- * @new: new node
- * Return: pointer
+ * find_loop - searches for loops
+ * @head: head
+ * Return: pointer or NULL
 */
-listint_t **r(listint_t **list, size_t size, listint_t *new)
+listint_t *find_loop(listint_t *head)
 {
-	listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
+	listint_t *end, *ptr;
+	if (head == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		newlist[i] = list[i];
-	}
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
 
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
 /**
  * free_listint_safe - frees a list
  * @h: head
@@ -36,24 +30,32 @@ listint_t **r(listint_t **list, size_t size, listint_t *new)
 */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i, size = 0;
-	listint_t **list = NULL;
+	size_t size;
+	int loop = 1;
+	listint_t *next, *loopn;
 
-	while (*h != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopn = find_loop(*h);
+	for (size = 0; (*h != loopn || loop) && *h != NULL; *h = next)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (*h == list[i])
-			{
-				free(list);
-				*h = NULL;
-				return (size);
-			}
-		}
 		size++;
-		list = r(list, size, *h);
-		(*h) = (*h)->next;
+		next = (*h)->next;
+		if (*h == loopn && loop)
+		{
+			if (loopn == loopn->next)
+			{
+				free(*h);
+				break;
+			}
+			size++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
 	}
-	free(list);
+	*h = NULL;
 	return (size);
 }
