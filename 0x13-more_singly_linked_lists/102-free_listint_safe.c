5b16a599340149c974a93527a9005f5ea2222be2
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-01 09:37:09 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * _r - reallocates memory
+ * @list: old list
+ * @size: size of the list
+ * @new: new node
+ * Return: pointer
+*/
+listint_t **r(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		return (NULL);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		newlist[i] = list[i];
+	}
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * free_listint_safe - frees a list
+ * @h: head
+ * Return: size
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i, size = 0;
+	listint_t **list = NULL;
+
+	while (*h != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (*h == list[i])
+			{
+				free(list);
+				*h = NULL;
+				return (size);
+			}
+		}
+		size++;
+		list = r(list, size, *h);
+		(*h) = (*h)->next;
+	}
+	free(list);
+	*h = NULL;
+	return (size);
+}
