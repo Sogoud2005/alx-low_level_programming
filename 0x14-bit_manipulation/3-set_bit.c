fce3c14bd3112befd8640cbb75fe34169d2eb4c3
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-03 13:44:22 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int m = 1 << index;
 
-	while (n)
-	{
-		*n = (*n & ~m) | (1 << index);
-		return (1);
-	}
-	return (-1);
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	
+	*n = (*n & ~m) | (1 << index);
+	return (1);
 }
