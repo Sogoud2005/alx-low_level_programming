c3a18bf27b651e93fee16e6a0dd528c5ef897c83
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-07 07:23:08 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+
 /**
  * main - copies content from one file to another
  * @argc: argc
@@ -15,33 +20,33 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, USAGE);
 		exit(97);
 	}
 	fd2 = open(argv[1], O_RDONLY);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
 		exit(98);
 	}
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 	}
 	while ((b = read(fd2, buff, READ_BUF_SIZE)) > 0)
 		if (write(fd, buff, b) != b)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 	if (b == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
 		exit(98);
 	}
 	fd = close(fd);
 	fd2 = close(fd2);
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd2), exit(100);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd2), exit(100);
 	return (EXIT_SUCCESS);
 }
