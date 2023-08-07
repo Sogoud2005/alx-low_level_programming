2da64e322a7135fcbdd8c554a82e1db3dda3ee00
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-07 11:16:17 -0700

    file i/o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,23 @@
 
 #define READ_BUF_SIZE 1024
 
+typedef struct {
+        unsigned char   e_ident[EI_NIDENT];
+        Elf64_Half      e_type;
+        Elf64_Half      e_machine;
+        Elf64_Word      e_version;
+        Elf64_Addr      e_entry;
+        Elf64_Off       e_phoff;
+        Elf64_Off       e_shoff;
+        Elf64_Word      e_flags;
+        Elf64_Half      e_ehsize;
+        Elf64_Half      e_phentsize;
+        Elf64_Half      e_phnum;
+        Elf64_Half      e_shentsize;
+        Elf64_Half      e_shnum;
+        Elf64_Half      e_shstrndx;
+} Elf64_Ehdr;
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
