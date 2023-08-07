6d2ce02aaa1f7228ce09c1fbdc530b801354cd77
Sogoud2005 <Sogoud2005@gmail.com>
2023-08-07 07:07:07 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	fd2 = open(argv[1], O_RDONLY);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
 		exit(98);
 	}
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (b == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
 		exit(98);
 	}
 	fd = close(fd);
