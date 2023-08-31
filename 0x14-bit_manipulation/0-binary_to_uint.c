a2b0a717aef418d35ddb9b1a6f3ac889ff12ba59
Sogoud2005 <sogoud2005@gmail.com>
2023-08-31 09:36:55 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,4 @@
 #include "main.h"
-/**
- * _strlen - gets string length
- * @b: string
- * Return: strlen
-*/
-
-int _strlen(const char *b)
-{
-	int i = 0;
-
-	while (*b++)
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * check_bi - converts binary to decimal
- * @b: pointer to binary
- * Return: 1 or 0
-*/
-
-int check_bi(const char *b)
-{
-	int i = 0;
-
-	for (i = 0; i < _strlen(b); i++)
-	{
-		if (b[i] != 48 && b[i] != 49)
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 
 /**
  * binary_to_uint - converts binary to decimal
@@ -44,7 +7,6 @@ int check_bi(const char *b)
 */
 unsigned int binary_to_uint(const char *b)
 {
-	int num, last, base = 1;
 	unsigned int dec = 0;
 
 	if (!b)
@@ -52,21 +14,11 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	}
 
-	if (check_bi(b) == 0)
+	while (*b)
 	{
-		return (0);
-	}
-
-	num = atoi(b);
-
-	while (num)
-	{
-		last = num % 10;
-		num = num / 10;
-
-		dec += last * base;
-
-		base = base * 2;
+		if (*b != '0' && *b != '1')
+			return (0);
+		dec = dec * 2 + (*b++ - '0');
 	}
 
 	return (dec);
