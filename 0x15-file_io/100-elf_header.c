36241a9d6122302312d50ec62442bf41ea5893db
Sogoud2005 <sogoud2005@gmail.com>
2023-09-04 12:50:56 -0700

    File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -129,4 +129,95 @@ void more_osabi(Elf64_Ehdr h)
 		case ELFOSABI_OPENBSD	
 			printf("UNIX - OpenBSD");
 		break;
+		case ELFOSABI_STANDALONE
+			printf("Standalone App");
+		break;
+		case ELFOSABI_ARM
+			printf("ARM");
+		break;
+		default:
+			printf("<unknown: %x>", h.eident[EI_OSABI]);
+			break;
+	}
+}
+/**
+ * print_abi - prints abi
+ * @h: elf struct
+*/
+void print_abi(Elf64_Ehdr h)
+{
+	printf("  ABI Version:                       %d\n",
+			h.e_ident[EI_ABIVERSION]);
+}
+/**
+ * print_type - prints type
+ * @h: elf struct
+*/
+void print_type(Elf64_Ehdr h)
+{
+	char *p = (char *)&h.e_type;
+	int i = 0;
 
+	printf("  Type:                              ");
+	if (h.e_ident[EI_DATA] == ELFDATA2MSB)
+		i = 1;
+
+	switch (p[i])
+	{
+		case ET_NONE
+			printf("NONE (None)");
+		break;
+		case ET_REL
+			printf("REL (Relocatable file)");
+		break;
+		case ET_EXEC
+			printf("EXEC (Executable file)");
+		break;
+		case ET_DYN
+			printf("DYN (Shared object file)");
+		break;
+		case ET_CORE
+			printf("CORE (Core file)");
+		break;
+		default:
+			printf("<unknown: %x>", p[i]);
+		break;
+	}
+	printf("\n");
+}
+/**
+ * print_entry - prints entry point
+ * @h: elf struct
+*/
+void print_entry(Elf64_Ehdr h)
+{
+	char *p = (char *)&h.e_ident;
+	int i = 0, len = 0;
+
+	printf("  Entry point address:               ");
+	if (h.e_ident[EI_DATA] != ELFDATA2MSB)
+	{
+		i = h.e_ident[EI_CLASS] == ELFCLASS64 ? 7 : 3;
+		while (p[i])
+		{
+			i--;
+		}
+		printf("%x", p[i--]);
+		for (; i >= 0; i--)
+		{
+			printf("%02x", p[i]);
+		}
+		printf("\n");
+	}
+	else
+	{
+		i = 0;
+		len = h.e_ident[EI_CLASS] == ELFCLASS64 ? 7 : 3;
+		while (!p[i])
+			i++;
+		printf("%x", p[i++]);
+		for (; i <= len)
+			printf("%02x", p[i]);
+		printf("\n");
+	}
+}
