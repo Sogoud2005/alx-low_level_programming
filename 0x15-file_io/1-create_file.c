6d2fed78dd1830292cc9b078cd66d200336e9856
Sogoud2005 <sogoud2005@gmail.com>
2023-09-04 10:26:34 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t l, len = _strlen(text_content);
 	int fd;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
